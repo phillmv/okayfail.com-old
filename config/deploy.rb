5d06d59b5b563fc30b3a78365f3bcae04afb3f53
Max Veytsman <maxim@ontoillogical.com>
2013-01-31 15:26:55 -0500

    Don't need to run bundle install explicitly because of rvm bundler gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,8 +21,7 @@ namespace :deploy do
   end
 end
 
-after "deploy:restart", "deploy:bundle"
-after "deploy:bundle", "deploy:run_jekyll"
+after "deploy:restart", "deploy:run_jekyll"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
