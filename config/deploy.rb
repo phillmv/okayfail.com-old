36d3cc3ba13ffd1de5fa2ac82e8162b202d82a98
Max Veytsman <maxim@ontoillogical.com>
2013-01-31 15:23:52 -0500

    Added Gemfile, updated deploy.rb to work with new server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,28 @@
-set :rvm_ruby_string, '1.9.2'                     # Or:
-#set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"") # Read from local system
-
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-require "rvm/capistrano"                               # Load RVM's capistrano plugin.
-
+require "bundler/capistrano"
+require "rvm/capistrano"
+set :rvm_ruby_string, 'ruby-1.9.3'
+set :rvm_type, :user
 set :application, "okayfail.com"
 set :repository,  "git@github.com:phillmv/okayfail.com.git"
 set :use_sudo, false
 set :scm, :git
-set :deploy_to, "/var/www/#{application}"
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
 
+set :user, "deploy"
 role :web, "okayfail.com"
 
 namespace :deploy do
+  task :bundle do
+    run "cd #{current_path} && bundle install"
+  end
   task :run_jekyll do
-    run "cd #{current_path} && jekyll"
+    run "cd #{current_path} && bundle exec jekyll"
   end
 end
 
-after "deploy:restart", "deploy:run_jekyll"
+after "deploy:restart", "deploy:bundle"
+after "deploy:bundle", "deploy:run_jekyll"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
